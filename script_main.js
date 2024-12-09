08247929b8ca2a996a124f6f7faa058a117c91f4
Dzmitry Stashkevich <dimarehfeldt@icloud.com>
2024-12-09 12:29:31 +0100

    lodingspinner/search/Overlay

--- script_main.js
+++ script_main.js
@@ -1,59 +1,126 @@
-let currentOffset = 1; // Start mit ID 1
-let limit = 20; // Anzahl der Pokémon, die pro Klick geladen werden kann jederzeit geändertwerden 
-let totalPokemons = 1309; // Gesamtzahl der Pokémon, die es gibt
+let currentOffset = 1; 
+let limit = 40; 
+let totalPokemons = 1309; 
+let loadedPokemons = [];
+let myVar;
 
-// Funktion, die durch den Button-Klick ausgelöst wird
-function loadMorePokemon() {
-  fetchPokedexJson();
+
+function init(){
+  currentNames = totalPokemons;
+  fetchPokedexJson();  
+  overlayContent(pokemon);
 }
 
 async function fetchPokedexJson() {
-  for (let i = currentOffset; i<currentOffset + limit && i <= totalPokemons; i++) { 
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-    let pokemon = await response.json(); // API-Antwort in JSON umwandeln
-    renderPokemon(pokemon);// Direkt das Pokémon anzeigen
-    console.log(pokemon);
-    
-  }
-  //stopt das laden wenn currentifset über dem limit liegt. 
-  currentOffset += limit; // Offset um den wert erhöhen
-    // Die Anzeige des Ladefortschritts aktualisieren
+
+    for (let i = currentOffset; i < currentOffset + limit && i <= totalPokemons; i++) {
+      let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      let pokemon = await response.json(); 
+      loadedPokemons.push(pokemon);
+      renderPokemon(pokemon); 
+      overlayContent(pokemon); 
+      console.log(pokemon);
+    }
+    currentOffset += limit;
     updatePokemonCount();
   }
 
- 
-function renderPokemon(pokemon) {
-  let pokemonsRef = document.getElementById('condent-cards');
+  function searchPokemon() {
+    let search = document.getElementById('search').value.trim().toLowerCase();
+    let content = document.getElementById('condent-cards');
+    document.getElementById('load-more').classList.add('d-none');
+    content.innerHTML = '';
+    if (search.length < 3) {
+        content.innerHTML = '<p>Bitte geben Sie mindestens 3 Buchstaben für die Suche ein!</p>';
+        document.getElementById('load-more').classList.remove('d-none');
+        return; 
+    }
+    if (loadedPokemons.length === 0) {
+        content.innerHTML = '<p>Es wurden noch keine Pokémon geladen. Bitte lade zuerst Pokémon!</p>';
+        return;
+    }
 
-  // Bestimmen der CSS-Klasse basierend auf dem Typ des Pokémon (erste Type)
-  const typeClass = pokemon.types[0].type.name;
-  // Überprüfen, ob ein zweiter Typ existiert
-  const hasSecondClass = pokemon.types.length > 1;
-  const secondClass = hasSecondClass ? pokemon.types[1].type.name : ''; 
-  //() in HTML setzte ich es in ein (ternären)operator hat es eine seconderklasse? wenn ja einsetzten wenn nein : ''dann freibleiben.)
- //syntax= condition ? exprIfTrue : exprIfFalse
+    let results = loadedPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search));
+    if (results.length > 0) {
+        results.forEach(pokemon => renderPokemon(pokemon));
+    } else {
+        content.innerHTML = '<p>Kein Pokémon gefunden, das den Suchbegriff enthält.</p>';
+    }
+    document.getElementById('load-more').classList.remove('d-none');
+}
+async function searchThroughAllPokemons(search) {
+    let results = [];
+    for (let i = 1; i <= totalPokemons; i++) {
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        let pokemon = await response.json();
+        if (pokemon.name.toLowerCase().includes(search)) {
+            results.push(pokemon);
+        }
+    }
+    return results;
+}
+
+
+function renderPokemon(pokemon) {
+
+  let pokemonsRef = document.getElementById('condent-cards');
+  let typeClass = pokemon.types[0].type.name;
+  let hasSecondClass = pokemon.types.length > 1;
+  let secondClass = hasSecondClass ? pokemon.types[1].type.name : ''; 
 
   pokemonsRef.innerHTML += `
-    <div class="poke-cards .example-1 bo_${typeClass}">
+    <div class="poke-cards bo_${typeClass}">
       <div class="header-cards">
         <p>#${pokemon.id}</p>
         <p><strong>${pokemon.name}</strong></p>
       </div>
-      <img  
-        class="card-img "
-        src="${pokemon.sprites.other.dream_world.front_default}"
-        alt="${pokemon.name}"
-      />
+      <img onclick="toggleOverlay(),current(${pokemon.id-1})"
+                class="card-img"
+                src="${pokemon.sprites.other.dream_world.front_default}" 
+                alt="${pokemon.name}" 
+            />
       <div class="card-icons ${typeClass} ">
-        <!-- Dynamisches Hinzufügen von Icons, basierend auf dem Typ -->
-        <img class="type-icon" src="./assets/icons/typeIcon_${typeClass}.png"alt="${typeClass} icon">
+        <img class="type-icon" title="${typeClass}"src="./assets/icons/typeIcon_${typeClass}.png"alt="${typeClass} icon">
           ${hasSecondClass ? `<img class="type-icon" src="./assets/icons/typeIcon_${secondClass}.png" alt="${secondClass} icon">` : ''}
     </div>
   `;
+  
  }
- // Funktion zum Aktualisieren der Anzeige der geladenen Pokémon // Math.min wie in der Quizapp 
 function updatePokemonCount() {
   const pokemonCountSpan = document.getElementById('pokemon-count');
   pokemonCountSpan.textContent = `${currentOffset}von ${Math.min(totalPokemons)} Pokémon geladen.`;
 }
+
+function loadMorePokemon() {
+  loadingspinner(); 
+  fetchPokedexJson();
+}
+
+function loadingspinner() {
+  let loader = document.getElementById("loader");
+  let loadMoreButton = document.getElementById("load-more");
+  let content = document.getElementById("myDiv");
  
+
+    loader.style.display = "block";
+    loadMoreButton.disabled = true;
+    content.style.display = "none";
+  
+  myVar = setTimeout(showPage, 1000);
+}
+function showPage() {
+  let loader = document.getElementById("loader");
+  let content = document.getElementById("myDiv");
+  let loadMoreButton = document.getElementById("load-more");
+
+  loader.style.display = "none";
+  content.style.display = "block";
+  loadMoreButton.disabled = false;
+}
+
+window.onload = function () {
+  loadingspinner();
+  setTimeout(() => {
+    fetchPokedexJson();
+  }, 1000); 
+};
--- script_overlay.js
+++ script_overlay.js
@@ -1,5 +1,56 @@
-function toggleOverlay() {
-    let overlayRef = document.getElementById('overlay')
+let currentIndex = 0;
 
-    overlayRef.classList.toggle('d_none')
+function overlayContent(pokemon) {
+    let overlayPokeContainer = document.getElementById("overlayBigCards");
+
+    overlayPokeContainer.innerHTML = `
+        <div class="bo_${pokemon.types[0].type.name}">
+            <div class="header-overlaybigcards">
+                <p>#${pokemon.id}</p>
+                <p><strong>${pokemon.name}</strong></p>
+            </div>
+            <img  
+                class="overlaybigcard-img"
+                src="${pokemon.sprites.other.dream_world.front_default}" 
+                alt="${pokemon.name}" 
+            />
+            <div class="overlay-details">
+                <p>Typ: ${pokemon.types.map(t => t.type.name).join(", ")}</p>
+                <p>Höhe: ${(pokemon.height / 10).toFixed(1)} m</p>
+                <p>Gewicht: ${(pokemon.weight / 10).toFixed(1)} kg</p>
+            </div>
+            <div class="nav-button">
+                <button onclick="back()">←</button>
+                <button onclick="next()">→</button> 
+            </div>
+        </div>
+    `;
 }
+function bubblingProtextin(event) {
+    event.stopPropagation();
+}
+function toggleOverlay() {
+    let overlayRef = document.getElementById("overlay");
+    overlayRef.classList.toggle("d_none");
+}
+function next() {
+    if (loadedPokemons.length > 0) {
+        currentIndex = (currentIndex + 1) % loadedPokemons.length; 
+        overlayContent(loadedPokemons[currentIndex]); 
+    }
+}
+function back() {
+    if (loadedPokemons.length > 0) {
+        currentIndex = (currentIndex - 1 + loadedPokemons.length) % loadedPokemons.length;
+        overlayContent(loadedPokemons[currentIndex]); 
+    }
+}
+function current(Poke){
+    currentIndex = Poke
+    overlayContent(loadedPokemons[currentIndex])
+}
+
+
+
+
+
